a2dba238d6b2a201c3d69d88a3057d50d9c8de0f
HoangViet <phanhoangviet1004@gmail.com>
2025-07-14 15:44:04 +0700

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ElementInspector } from "element-prompt-builder";
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      {process.env.DEV && <ElementInspector />}
+      <p>Start prompting (or editing) to see magic happen :)</p>
+    </div>
+  )
+}
+
+export default App
